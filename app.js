94d2d58ce91e7abb3eb8852e90ba498500981a30
IgnasBarakauskas <b.ignas05@gmail.com>
2021-02-21 18:56:01 +0100

    parser issues solved

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const mongoose = require("mongoose")
 require("dotenv").config()
-const app = express();
+const bodyParser = require("body-parser")
 const usersRoute = require("./routes/users");
 const groupsRoute = require("./routes/groups");
-const PORT = process.env.PORT || 3000
 
+const PORT = process.env.PORT || 3000
+const app = express();
+app.disable("x-powered-by");
+app.use(bodyParser.json())
 app.use('/users', usersRoute)
 
 app.use('/groups', groupsRoute)
@@ -13,7 +16,6 @@ app.use('/groups', groupsRoute)
 app.get('/', (req,res) =>{
     res.send("Hello world!!!")
 })
-console.log("Test",process.env.DB_LOGIN)
 mongoose.connect(
  `mongodb+srv://${process.env.DB_LOGIN}:${process.env.DB_PASSWORD}@findasportbuddy.ad0wh.mongodb.net/findASportBuddy?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true },
