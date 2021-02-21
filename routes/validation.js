9ac43306ba543c539014df9287b6ca40c352cf09
IgnasBarakauskas <b.ignas05@gmail.com>
2021-02-21 14:18:35 +0100

    Registration API

--- routes/validation.js
+++ routes/validation.js
@@ -2,14 +2,10 @@ const Joi = require("@hapi/joi");
 
 const userValidation = data => {
     const schema = Joi.object({
-        fullname: Joi.string().min(6).required(),
+        fullName: Joi.string().min(6).required(),
         email: Joi.string().min(6).required().email(),
         password: Joi.string().min(6).required(),
         birthdayDate: Joi.date().required(),
-        height: Joi.number(),
-        weight: Joi.number(),
-        latitude: Joi.number(),
-        longtitude: Joi.number(),
     });
     return schema.validate(data)
 }
