42d65a031d21081274eef36a99b9c74424c6a6bb
IgnasBarakauskas <b.ignas05@gmail.com>
2021-04-25 19:41:10 +0200

    updated file size

--- app.js
+++ app.js
@@ -10,18 +10,16 @@ const PORT = process.env.PORT || 3000
 const app = express();
 app.use(cors());
 app.disable("x-powered-by");
-app.use(bodyParser.json())
+app.use(bodyParser.json({limit: '3mb', extended: true}))
 app.use('/users', usersRoute)
 app.use('/groups', groupsRoute)
 app.use('/courts', courtsRoute)
-
 app.get('/', (req,res) =>{
     res.send("Hello world!!!")
 })
 mongoose.connect(
  `mongodb+srv://${process.env.DB_LOGIN}:${process.env.DB_PASSWORD}@findasportbuddy.ad0wh.mongodb.net/findASportBuddy?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true },
-    () => console.log("connected to DB")
   );
 
 app.listen(PORT)
